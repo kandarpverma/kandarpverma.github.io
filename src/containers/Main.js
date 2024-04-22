790dbd051139775ee24d3a731ed0d50f870d407f
Kandarp Verma <kandarp.verma@gmail.com>
2024-04-22 16:01:18 +0530

    github profile added

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -5,7 +5,6 @@ import Skills from "./skills/Skills";
 import StackProgress from "./skillProgress/skillProgress";
 import WorkExperience from "./workExperience/WorkExperience";
 import Projects from "./projects/Projects";
-import StartupProject from "./StartupProjects/StartupProject";
 import Achievement from "./achievement/Achievement";
 import Education from "./education/Education";
 import ScrollToTopButton from "./topbutton/Top";
@@ -52,8 +51,7 @@ const Main = () => {
             <Education />
             <WorkExperience />
             <Projects />
-            <StartupProject />
-            <Achievement />
+            {/* <Achievement /> */}
             <Profile />
             <ScrollToTopButton />
           </>
